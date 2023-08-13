0d52cae81917de256e4c4f6e1a302411685f779e
StuXet <mark489@live.ru>
2023-08-13 12:51:53 +0300

    Game. Log & Window

--- SimpleOpenGlEngine/Main.cpp
+++ SimpleOpenGlEngine/Main.cpp
@@ -2,19 +2,26 @@
 
 int main(int argc, char** argv)
 {
+	//Screen dimention
 	const int SCREEN_WIDTH = 800;
 	const int SCREEN_HEIGHT = 450;
 
+	//Window pointer
 	SDL_Window* window = nullptr;
 
+	//Initialize SDL
 	SDL_Init(SDL_INIT_VIDEO);
 
+	//Create Window
 	window = SDL_CreateWindow("First Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOWEVENT_SHOWN);
 
+	//Wait 2 seconds
 	SDL_Delay(2000);
 
+	//Destroy Window
 	SDL_DestroyWindow(window);
 
+	//Qiut SDL
 	SDL_Quit();
 
 	return 0;
