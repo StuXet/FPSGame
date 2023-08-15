f368eb39fd840e5dd5948ae2ba85beebcb664eca
StuXet <mark489@live.ru>
2023-08-15 10:40:50 +0300

    Bug fix, Animation and Scrolling

--- SimpleOpenGlEngine/Game.cpp
+++ SimpleOpenGlEngine/Game.cpp
@@ -1,66 +1,71 @@
 #include "Game.h"
-
-#include "Assets.h"
+#include "Actor.h"
+#include "SpriteComponent.h"
+#include "AnimSpriteComponent.h"
 #include "Timer.h"
+#include "Assets.h"
+#include "BackgroundSpriteComponent.h"
 
 bool Game::initialize()
 {
 	bool isWindowInit = window.initialize();
 	bool isRendererInit = renderer.initialize(window);
-
-	int windowWidth = window.getWidth();
-	int windowHeight = window.getHeigth();
-
-	return isWindowInit && isRendererInit; // return bool && bool && bool ...to detect error
-}
-
-void Game::loop()
-{
-	Timer timer;
-	float dt = 0;
-	while (isRunning)
-	{
-		float dt = timer.computeDeltaTime() / 1000.0f;
-		processInput();
-		//update();
-		update(dt);
-		render();
-		timer.delayTime();
-	}
+	return isWindowInit && isRendererInit; // Return bool && bool && bool ...to detect error
 }
 
 void Game::load()
 {
-	Assets::loadTexture(renderer, "Res\Ship01.png", "ship01");
-	auto actor = new Actor();
-	auto sprite = new SpriteComponent(actor, Assets::getTexture("ship01"));
-	actor->setPosition(Vector2{ 100,100 });
-}
+	// Load textures
+	Assets::loadTexture(renderer, "Res\\Ship01.png", "Ship01");
+	Assets::loadTexture(renderer, "Res\\Ship02.png", "Ship02");
+	Assets::loadTexture(renderer, "Res\\Ship03.png", "Ship03");
+	Assets::loadTexture(renderer, "Res\\Ship04.png", "Ship04");
+	Assets::loadTexture(renderer, "Res\\Farback01.png", "Farback01");
+	Assets::loadTexture(renderer, "Res\\Farback02.png", "Farback02");
+	Assets::loadTexture(renderer, "Res\\Stars.png", "Stars");
 
-void Game::unload()
-{
-	//Delete actors
-	//Because ~Actor calls RemoveActor, have to use a different style loop
-	while(!actors.empty())
-	{
-		delete actors.back();
-	}
+	// Single sprite
+	/*
+	Actor* actor = new Actor();
+	SpriteComponent* sprite = new SpriteComponent(actor, Assets::getTexture("Ship01"));
+	actor->setPosition(Vector2{ 100, 100 });
+	*/
 
-	//Resources
-	Assets::clear();
-}
+	// Animated sprite
+	vector<Texture*> animTextures{
+		&Assets::getTexture("Ship01"),
+		&Assets::getTexture("Ship02"),
+		&Assets::getTexture("Ship03"),
+		&Assets::getTexture("Ship04"),
+	};
+	Actor* ship = new Actor();
+	AnimSpriteComponent* animatedSprite = new AnimSpriteComponent(ship, animTextures);
+	ship->setPosition(Vector2{ 100, 300 });
 
 
-void Game::close()
-{
-	renderer.close();
-	window.close();
-	SDL_Quit();
+	// Background
+	// Create the "far back" background
+	vector<Texture*> bgTexsFar{
+		&Assets::getTexture("Farback01"),
+		&Assets::getTexture("Farback02")
+	};
+	Actor* bgFar = new Actor();
+	BackgroundSpriteComponent* bgSpritesFar = new BackgroundSpriteComponent(bgFar, bgTexsFar);
+	bgSpritesFar->setScrollSpeed(-100.0f);
+
+	// Create the closer background
+	Actor* bgClose = new Actor();
+	std::vector<Texture*> bgTexsClose{
+		&Assets::getTexture("Stars"),
+		&Assets::getTexture("Stars")
+	};
+	BackgroundSpriteComponent* bgSpritesClose = new BackgroundSpriteComponent(bgClose, bgTexsClose, 50);
+	bgSpritesClose->setScrollSpeed(-200.0f);
 }
 
 void Game::processInput()
 {
-	//SDL Event
+	// SDL Event
 	SDL_Event event;
 	while (SDL_PollEvent(&event))
 	{
@@ -71,10 +76,9 @@ void Game::processInput()
 			break;
 		}
 	}
-	//Keyboard state
+	// Keyboard state
 	const Uint8* keyboardState = SDL_GetKeyboardState(nullptr);
-
-	//Escape : quit game
+	// Escape: quit game
 	if (keyboardState[SDL_SCANCODE_ESCAPE])
 	{
 		isRunning = false;
@@ -83,22 +87,22 @@ void Game::processInput()
 
 void Game::update(float dt)
 {
-	//Update actors
+	// Update actors 
 	isUpdatingActors = true;
-	for (auto actor: actors)
+	for (auto actor : actors)
 	{
 		actor->update(dt);
 	}
 	isUpdatingActors = false;
 
-	//MoveActors to actors
-	for(auto pendingActor: pendingActors)
+	// Move pending actors to actors
+	for (auto pendingActor : pendingActors)
 	{
 		actors.emplace_back(pendingActor);
 	}
 	pendingActors.clear();
 
-	//Delete dead actors
+	// Delete dead actors
 	vector<Actor*> deadActors;
 	for (auto actor : actors)
 	{
@@ -111,6 +115,47 @@ void Game::update(float dt)
 	{
 		delete deadActor;
 	}
+}
+
+void Game::render()
+{
+	renderer.beginDraw();
+	renderer.draw();
+	renderer.endDraw();
+}
+
+void Game::loop()
+{
+	Timer timer;
+	float dt = 0;
+	while (isRunning)
+	{
+		float dt = timer.computeDeltaTime() / 1000.0f;
+		processInput();
+		update(dt);
+		render();
+		timer.delayTime();
+	}
+}
+
+void Game::unload()
+{
+	// Delete actors
+	// Because ~Actor calls RemoveActor, have to use a different style loop
+	while (!actors.empty())
+	{
+		delete actors.back();
+	}
+
+	// Resources
+	Assets::clear();
+}
+
+void Game::close()
+{
+	renderer.close();
+	window.close();
+	SDL_Quit();
 }
 
 void Game::addActor(Actor* actor)
@@ -127,11 +172,11 @@ void Game::addActor(Actor* actor)
 
 void Game::removeActor(Actor* actor)
 {
-	//Erase actor from the two vectors
+	// Erase actor from the two vectors
 	auto iter = std::find(begin(pendingActors), end(pendingActors), actor);
 	if (iter != end(pendingActors))
 	{
-		//Swap to end of vector and pop off(avoid erase copies)
+		// Swap to end of vector and pop off (avoid erase copies)
 		std::iter_swap(iter, end(pendingActors) - 1);
 		pendingActors.pop_back();
 	}
@@ -141,14 +186,4 @@ void Game::removeActor(Actor* actor)
 		std::iter_swap(iter, end(actors) - 1);
 		actors.pop_back();
 	}
-}
-
-
-void Game::render()
-{
-	renderer.beginDraw();
-
-	renderer.draw();
-
-	renderer.endDraw();
 }
